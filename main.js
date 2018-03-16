38ce2536fbf414ae0657e67e7635cf69848737c7
Shari Straker <sharistraker@Sharis-MacBook-Pro.local>
2018-03-16 12:08:47 -0400

    adding content

---
+++ main.js
@@ -0,0 +1,14 @@
+//decalring all variables//
+
+
+//non-government organization assistance for families//
+var ngoAssistance = [
+  "help unlimited", "new start", "better tomorrow", "home safe", "new home"
+];
+
+//monthly expenses for family members. one person is allotted $800, family of two is allotted $900, family of three is allotted $1000, family of four is allotted $1100 and a family of five or more has a fixed $1200//
+
+var monthly.Expenses.Family = {
+  family:["One Person", "Two People", "Three People", "Four People", "Four People", "Five People"]
+  expenses: ["800", "950", "1000", "1100", "1200"]
+};
